82a389dee3ae8b2dc68df1c2ec5c9e3d7df6f9d2
Evan Grantham-Brown <egrantha@nd.edu>
2014-03-24 16:02:34 -0400

    Fixing again.

--- app/models/security_goon.rb
+++ app/models/security_goon.rb
@@ -9,12 +9,9 @@ class SecurityGoon
   	end
   	proxy_ticket = CASClient::ProxyTicket.new(params[:ticket],params[:service])
     validate_result = CASClient::Frameworks::Rails::Filter.client.validate_proxy_ticket(proxy_ticket)
-    if !validate_result.has_key?("success")
+    if !validate_result.success
       return nil
     end
-    if !validate_result["success"]
-      return nil
-    end
-    return validate_result["user"]
+    return validate_result.user
   end
 end
