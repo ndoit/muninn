4b5afc06fd0bcdd8aa22f46ca3069946b607cf1b
Ryan Snodgrass <ryansnodgrass.dev@gmail.com>
2014-12-05 19:18:56 +0000

    cleaned space/tabs in guide_controller.rb

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -17,81 +17,81 @@ class GraphController < ApplicationController
   end
   
   def create
-  	LogTime.info "Instantiating ModelRepository."
-  	repository = get_model_repository
-  	
-  	LogTime.info "Writing to database creating node."
+    LogTime.info "Instantiating ModelRepository."
+    repository = get_model_repository
+    
+    LogTime.info "Writing to database creating node."
     output = repository.write(params, true, session[:cas_user])
-  	if output[:success]
-  	  render :status => 200, :json => output
-  	else
+    if output[:success]
+      render :status => 200, :json => output
+    else
       render :status => 500, :json => output
-  	end
+    end
   end
   
   def update
     LogTime.info "Instantiating ModelRepository."
-  	repository = get_model_repository
-  	
-  	LogTime.info "Writing to database."
+    repository = get_model_repository
+    
+    LogTime.info "Writing to database."
     output = repository.write(params, false, session[:cas_user])
-  	if output[:success]
-  	  render :status => 200, :json => output
-  	else
+    if output[:success]
+      render :status => 200, :json => output
+    else
         render :status => 500, :json => output
-  	end
+    end
   end
   
   def show
     #LogTime.info "Playing with databases."
     #EventLogger.log_event
     
-  	LogTime.info "Instantiating ModelRepository."
-	  repository = get_model_repository
-	
+    LogTime.info "Instantiating ModelRepository."
+    repository = get_model_repository
+  
     LogTime.debug "Processing read request."
     output = repository.read(params, session[:cas_user])
     #output[:cas_user] = session[:cas_user]
 
     output[:validated_user] = SecurityGoon.who_is_this(params)
-	
+  
     LogTime.debug "Rendering output."
-	  if output[:success]
+    if output[:success]
       render :status => 200, :json => output
-	  else
+    else
       render :status => 500, :json => output
-	  end
+    end
   end
   
   def destroy
-  	LogTime.info "Instantiating ModelRepository."
-	  repository = get_model_repository
-	
+    LogTime.info "Instantiating ModelRepository."
+    repository = get_model_repository
+  
     LogTime.debug "Processing delete request."
     output = repository.delete(params, session[:cas_user])
-	
+  
     LogTime.debug "Rendering output."
-  	if output[:success]
+    if output[:success]
         render :status => 200 ,:json => { success: true, message: "Sucessfully deleted the node" }
-  	else
+    else
         render :status => 500 ,:json => { success: false, message: "Node has not been deleted." }
-  	end
+    end
   end
 
   def search
-  	if params.has_key?(:query_string)
-  	  query_string = params[:query_string]
-  	else
+    if params.has_key?(:query_string)
+      query_string = params[:query_string]
+    else
       render :status => 500, :json => { success: false, message: "Enter a query string." }
       return
     end
 
     output = ElasticSearchIO.instance.search(URI.escape(query_string), @primary_label)
-  	if output[:success]
-  	  render :status => 200, :json => output
-  	else
+    if output[:success]
+      render :status => 200, :json => output
+    else
         render :status => 500, :json => output
-  	end
+    end
   end
 
   def index
